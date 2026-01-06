8d9d95515d15ff663d082f2f43cd1e2d69c3f2e7
mh bakhshipour <mh.bakhshipour@yahoo.com>
2026-01-06 18:57:56 +0330

    feat(feed): add suggested users section to feed page
    Include a list of suggested users in the feed view, showing recently joined users (excluding current user) to improve user discovery and engagement

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
+from accounts.models import User
 from relationships.models import Follow
 from .forms import PostForm
 from .models import Post
@@ -26,10 +27,14 @@ def feed(request):
     paginator = Paginator(posts, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+    suggested_users = (
+        User.objects.exclude(pk=request.user.pk)
+        .order_by("-date_joined")[:5]
+    )
     return render(
         request,
         "posts/feed.html",
-        {"form": form, "page_obj": page_obj},
+        {"form": form, "page_obj": page_obj, "suggested_users": suggested_users},
     )
 
 
